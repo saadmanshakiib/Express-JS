5c9ce4507cf537d94fcb283857f79d983c613d95
saadmanshakiib <saadman@Sadmans-MacBook-Air.local>
2026-02-22 23:45:43 +0600

    MiddleWire

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = express();
 
 app.listen(2000);
 
-app.get("/ok",(req,res)=>{
+const middleWire=(req,res,next)=>{
+console.log("Middle wire is ok");
+req.currentTime = new Date(Date.now());
+next();
+}
+
+
+app.get("/ok",middleWire,(req,res)=>{
+    console.log("I am Okay" + req.currentTime);
     res.send("I am good");
 })
+app.get("/about",middleWire,(req,res)=>{
+    console.log("About Path is good");
+    res.send("I am about path");
+})
+
